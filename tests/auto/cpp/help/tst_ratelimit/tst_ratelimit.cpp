f75b0bef06fa441c9a9e4c7c5d34eeed5b4787d2
Tasuku Suzuki <stasuku@gmail.com>
2013-06-19 21:45:02 +0900

    fix tst_ratelimit failure
    Change-Id: If07fb663b7ca6249c975244b340a09ce04be83ef
    Reviewed-on: http://cr.qtquick.me/708
    Tested-by: http://ci.qtquick.me/
    Reviewed-by: Tasuku Suzuki <stasuku@gmail.com>
    Tested-by: Tasuku Suzuki <stasuku@gmail.com>

--- tests/auto/cpp/help/tst_ratelimit/tst_ratelimit.cpp
+++ tests/auto/cpp/help/tst_ratelimit/tst_ratelimit.cpp
@@ -47,7 +47,7 @@ void tst_ratelimit::run()
     QVERIFY2(timeline.xrlRemaining() > 0, "AbstractTwitterModel::xrlRemaining()");
     //RateLimitReset of hometimeline is always within 15min(900sec).
     qDebug() << timeline.xrlReset() << QDateTime::currentDateTime().secsTo(timeline.xrlReset());
-    QVERIFY2(QDateTime::currentDateTime().secsTo(timeline.xrlReset()) < 60 * 15, "AbstractTwitterModel::xrlReset()");
+    QVERIFY2(QDateTime::currentDateTime().secsTo(timeline.xrlReset()) <= 60 * 15, "AbstractTwitterModel::xrlReset()");
 }
 
 QTEST_MAIN(tst_ratelimit)
