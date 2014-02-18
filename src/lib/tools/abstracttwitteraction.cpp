b2ad89ea520311fbb0449235d10d3a4706526167
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-02-18 18:02:42 +0900

    parce json when receive error

--- src/lib/tools/abstracttwitteraction.cpp
+++ src/lib/tools/abstracttwitteraction.cpp
@@ -1,6 +1,6 @@
 /* Copyright (c) 2012-2013 Twitter4QML Project.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -11,7 +11,7 @@
  *     * Neither the name of the Twitter4QML nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -173,12 +173,21 @@ void AbstractTwitterAction::Private::finished()
 void AbstractTwitterAction::Private::error(QNetworkReply::NetworkError error)
 {
     QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
-    DEBUG() << error << reply->errorString() << reply->url();
-    QVariantMap data;
-    data.insert("error", reply->errorString());
-    q->setData(data);
+    QByteArray networkReply = reply->readAll();
+    DEBUG() << networkReply << error << reply->errorString() << reply->url();
+
+    // Just don't do anything if we didn't get anything useful.
+    if(networkReply.isEmpty()) {
+        reply->deleteLater();
+        q->setLoading(false);
+        return;
+    }
+
+    // Let's parse received JSON contains error message
+    // https://dev.twitter.com/docs/error-codes-responses
+    parser.parse(networkReply);
     reply->deleteLater();
-    q->setLoading(false);
+//    q->setLoading(false);
 }
 
 void AbstractTwitterAction::Private::parsed(const QVariant &result)
